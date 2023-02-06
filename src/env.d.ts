a4d38831fe69e3f3998b01e588cdf694ab9c233b
Alejandro González <AlexTMjugador@users.noreply.github.com>
2023-02-06 15:16:47 +0100

    chore: run Prettier

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="@astrojs/image/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_SITE_TAGLINE: string;
+	readonly PUBLIC_SITE_TAGLINE: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+	readonly env: ImportMetaEnv;
 }
