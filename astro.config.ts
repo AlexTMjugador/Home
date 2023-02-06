4ee431ca0b28fe0f08e5ec8b9715875f6a768859
Alejandro González <AlexTMjugador@users.noreply.github.com>
2023-02-06 23:13:45 +0100

    chore: remove prefetch integration
    It does not seem very useful, at least for now.

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import prefetch from "@astrojs/prefetch";
 import robotsTxt from "astro-robots-txt";
 import webmanifest from "astro-webmanifest";
 import frontendistahtmlMinify from "@frontendista/astro-html-minify";
@@ -20,7 +19,6 @@ export default defineConfig({
 		image(),
 		sitemap(),
 		mdx(),
-		prefetch(),
 		robotsTxt(),
 		webmanifest({
 			name: "Alejandro's personal space",
