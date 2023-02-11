49e72f4d3337c7cff0d4fc920ad55875c28a4f1a
Alejandro González <AlexTMjugador@users.noreply.github.com>
2023-02-11 23:00:50 +0100

    feat: complete home page and utility components
    A color scheme for the page was decided, based on a monochromatic
    harmony where brown is the main color.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,16 @@ module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			sans: ["Finlandica", ...defaultTheme.fontFamily.sans],
+			sans: ["'Exo 2'", ...defaultTheme.fontFamily.sans],
+			mono: defaultTheme.fontFamily.mono,
+		},
+		colors: {
+			// Inspired by https://davidpiesse.github.io/tailwind-md-colours/
+			transparent: "transparent",
+			"amber-400-accent": "#ffc400",
+			"brown-400": "#8d6e63",
+			"brown-500": "#795548",
+			"amber-50": "#fff8e1",
 		},
 	},
 	plugins: [],
