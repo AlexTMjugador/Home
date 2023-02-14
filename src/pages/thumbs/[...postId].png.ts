96863bf7e9cc8f7b425655ef7eab0fc6dfd7dad2
Alejandro González <AlexTMjugador@users.noreply.github.com>
2023-02-14 19:25:10 +0100

    feat: add blog section skeleton and final blog OG image endpoint

--- src/pages/thumbs/[...postId].png.ts
+++ src/pages/thumbs/[...postId].png.ts
@@ -1,65 +1,65 @@
 import { OGImageRoute } from "astro-og-canvas";
+import { CollectionEntry, getCollection } from "astro:content";
 
-const loremIpsum =
-	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pulvinar tristique lectus eget pellentesque. Donec consectetur dolor nec hendrerit rhoncus. Proin nec sapien at arcu convallis tempus non eu erat. Vestibulum tincidunt neque egestas ante rutrum laoreet. Nullam at viverra nulla. Nam vitae erat augue. Ut et est venenatis, aliquet mi eu, rutrum nulla. Cras in risus lobortis, ultricies dui vitae, finibus massa. Suspendisse potenti. Phasellus pretium elementum elit, quis luctus orci aliquet id. Donec scelerisque convallis massa, sed interdum mauris tempus ac. Nullam aliquam at magna sit amet dictum.";
-
-const title = trimAndEllideAfterLastWord(loremIpsum);
-const description = trimAndEllideAfterLastWord(loremIpsum);
-
-const titleFontSize = fitFontSizeForText(title, 32, 72);
-const descriptionFontSize = fitFontSizeForText(
-	description,
-	26,
-	Math.min(38, titleFontSize)
+const blogPosts = await getCollection(
+	"blog",
+	(blogPost) => !blogPost.data.draft
 );
 
 export const { getStaticPaths, get } = OGImageRoute({
 	param: "postId",
-	// TODO generate static paths from the frontmatter data of blog posts
-	// collections. See:
-	// https://docs.astro.build/en/guides/content-collections/
-	pages: {
-		"post/3": {
+	pages: blogPosts.reduce((pagesObject, blogPost) => {
+		pagesObject[blogPost.slug] = blogPost;
+		return pagesObject;
+	}, {} as Record<string, CollectionEntry<"blog">>),
+	getSlug: (postSlug) => `post/${postSlug}`,
+	getImageOptions: (_, page: CollectionEntry<"blog">) => {
+		const title = trimAndEllideAfterLastWord(page.data.title);
+		const description = trimAndEllideAfterLastWord(page.data.description);
+
+		const titleFontSize = fitFontSizeForText(title, 32, 72);
+		const descriptionFontSize = fitFontSizeForText(
+			description,
+			26,
+			Math.min(38, titleFontSize)
+		);
+
+		return {
 			title: title,
 			description: description,
-		},
+			format: "PNG",
+			padding: 60,
+			fonts: ["https://api.fontsource.org/v1/fonts/exo-2/latin-400-normal.ttf"],
+			font: {
+				title: {
+					color: [255, 248, 225],
+					families: ["Exo 2"],
+					size: titleFontSize,
+					lineHeight: 1.3,
+					weight: "Bold",
+				},
+				description: {
+					color: [255, 248, 225],
+					families: ["Exo 2"],
+					size: descriptionFontSize,
+					lineHeight: 1.15,
+					weight: "Medium",
+				},
+			},
+			logo: {
+				path: "./src/images/favicon_light_og.png",
+			},
+			bgGradient: [
+				[141, 110, 99],
+				[121, 85, 72],
+			],
+			border: {
+				color: [255, 196, 0],
+				side: "inline-start",
+				width: 10,
+			},
+		};
 	},
-	getSlug: (postId) => postId,
-	getImageOptions: (_, page) => ({
-		title: page.title,
-		description: page.description,
-		format: "PNG",
-		padding: 60,
-		fonts: ["https://api.fontsource.org/v1/fonts/exo-2/latin-400-normal.ttf"],
-		font: {
-			title: {
-				color: [255, 248, 225],
-				families: ["Exo 2"],
-				size: titleFontSize,
-				lineHeight: 1.3,
-				weight: "Bold",
-			},
-			description: {
-				color: [255, 248, 225],
-				families: ["Exo 2"],
-				size: descriptionFontSize,
-				lineHeight: 1.15,
-				weight: "Medium",
-			},
-		},
-		logo: {
-			path: "./src/images/favicon_light_og.png",
-		},
-		bgGradient: [
-			[141, 110, 99],
-			[121, 85, 72],
-		],
-		border: {
-			color: [255, 196, 0],
-			side: "inline-start",
-			width: 10,
-		},
-	}),
 });
 
 function trimAndEllideAfterLastWord(
