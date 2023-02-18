6c0b1b7e33073a4cf12db5191caec86fe7ed6b58
Alejandro González <AlexTMjugador@users.noreply.github.com>
2023-02-18 15:23:27 +0100

    feat: add blog post search feature with Pagefind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -42,5 +42,26 @@ module.exports = {
 			}),
 		},
 	},
-	plugins: [require("@tailwindcss/typography")],
+	plugins: [
+		require("@tailwindcss/typography"),
+		// From https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574
+		function ({ addBase, theme }) {
+			function extractColorVars(colorObj, colorGroup = "") {
+				return Object.keys(colorObj).reduce((vars, colorKey) => {
+					const value = colorObj[colorKey];
+
+					const newVars =
+						typeof value === "string"
+							? { [`--color${colorGroup}-${colorKey}`]: value }
+							: extractColorVars(value, `-${colorKey}`);
+
+					return { ...vars, ...newVars };
+				}, {});
+			}
+
+			addBase({
+				":root": extractColorVars(theme("colors")),
+			});
+		},
+	],
 };
