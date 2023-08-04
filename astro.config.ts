fa6ecc52139cf7734a72b7e88531b819aa4bf8e9
Alejandro González <me@alegon.dev>
2023-08-04 13:33:02 +0200

    chore: bump dependencies, update HTML minifier integration config
    The minifier config updates are necessary for the new major version.

--- astro.config.ts
+++ astro.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
 		}),
 		pagefind(),
 		frontendistahtmlMinify({
-			htmlTerserMinifierOptions: {
+			html: {
 				decodeEntities: true,
 				removeComments: true,
 				sortAttributes: true,
