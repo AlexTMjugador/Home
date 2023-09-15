b7ebe5e696a48d9dbdcbd7e6803b30b2b25e473d
Alejandro González <me@alegon.dev>
2023-09-16 00:41:51 +0200

    refactor: move Tailwind CSS config to TypeScript
    This provides a more type-safe editing experience.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require("tailwindcss/defaultTheme");
+import type { Config } from "tailwindcss";
 
-/** @type {import("tailwindcss").Config} */
-module.exports = {
+import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
+import type {
+	PluginCreator,
+	RecursiveKeyValuePair,
+} from "tailwindcss/types/config";
+
+export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
@@ -18,7 +23,7 @@ module.exports = {
 			"amber-50": "#fff8e1",
 		},
 		extend: {
-			typography: ({ theme }) => ({
+			typography: ({ theme }: { theme: (_: string) => string }) => ({
 				DEFAULT: {
 					css: {
 						"--tw-prose-body": theme("colors.amber-50"),
@@ -43,10 +48,13 @@ module.exports = {
 		},
 	},
 	plugins: [
-		require("@tailwindcss/typography"),
+		typography,
 		// From https://gist.github.com/Merott/d2a19b32db07565e94f10d13d11a8574
 		function ({ addBase, theme }) {
-			function extractColorVars(colorObj, colorGroup = "") {
+			function extractColorVars(
+				colorObj: RecursiveKeyValuePair<string, string>,
+				colorGroup = "",
+			): RecursiveKeyValuePair<string, string> {
 				return Object.keys(colorObj).reduce((vars, colorKey) => {
 					const value = colorObj[colorKey];
 
@@ -62,6 +70,6 @@ module.exports = {
 			addBase({
 				":root": extractColorVars(theme("colors")),
 			});
-		},
+		} satisfies PluginCreator,
 	],
-};
+} satisfies Config;
